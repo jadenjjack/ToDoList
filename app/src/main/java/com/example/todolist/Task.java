c798e85695aa2cd4c1f6e20e2961d9fc24811b78
Jaden Jack <113564175+jadenjjack@users.noreply.github.com>
2024-02-25 20:49:12 -0500

    Created the task object and the getters and setters.

--- app/src/main/java/com/example/todolist/Task.java
+++ app/src/main/java/com/example/todolist/Task.java
@@ -1,4 +1,57 @@
 package com.example.todolist;
 
+import java.util.Calendar;
+
 public class Task {
+    private int taskID;
+    private String subject;
+    private String content;
+    private String priority;
+    private Calendar dueDate;
+
+    public Task() {
+        taskID = -1;
+        dueDate = Calendar.getInstance();
+    }
+
+    public int getTaskID() {
+        return taskID;
+    }
+
+    public void setTaskID(int taskID) {
+        this.taskID = taskID;
+    }
+
+    public String getSubject() {
+        return subject;
+    }
+
+    public void setSubject(String subject) {
+        this.subject = subject;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public String getPriority() {
+        return priority;
+    }
+
+    public void setPriority(String priority) {
+        this.priority = priority;
+    }
+
+    public Calendar getDueDate() {
+        return dueDate;
+    }
+
+    public void setDueDate(Calendar dueDate) {
+        this.dueDate = dueDate;
+    }
+
 }
