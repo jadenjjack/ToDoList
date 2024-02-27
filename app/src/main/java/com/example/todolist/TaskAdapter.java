1828d152c3942507e40c770642c7e46547bdc289
Jaden Jack <113564175+jadenjjack@users.noreply.github.com>
2024-02-27 12:27:09 -0500

    Made changes to taskList

--- app/src/main/java/com/example/todolist/TaskAdapter.java
+++ app/src/main/java/com/example/todolist/TaskAdapter.java
@@ -1,4 +1,121 @@
 package com.example.todolist;
 
-public class TaskAdapter {
+import android.content.Context;
+import android.text.format.DateFormat;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+import java.util.ArrayList;
+
+public class TaskAdapter extends RecyclerView.Adapter {
+    private ArrayList<Task> taskData;
+    private View.OnClickListener mOnItemClickListener;
+    private boolean isDeleting;
+    private Context parentContext;
+    public TaskAdapter(ArrayList<Task> taskData, Context context) {
+        this.taskData = taskData;
+        parentContext = context;
+    }
+
+    public class TaskViewHolder extends RecyclerView.ViewHolder {
+        public TextView tvSubject;
+        public TextView tvDueDate;
+        public TextView tvPriority;
+        public Button deleteButton;
+        public TaskViewHolder(@NonNull View itemView) {
+            super(itemView);
+            tvSubject = itemView.findViewById(R.id.textSubject);
+            tvDueDate = itemView.findViewById(R.id.textDueDate);
+            tvPriority = itemView.findViewById(R.id.textPriority);
+            deleteButton = itemView.findViewById(R.id.deleteTaskButton);
+            itemView.setTag(this);
+            itemView.setOnClickListener(mOnItemClickListener);
+        }
+        public TextView getTvSubject() {
+            return tvSubject;
+        }
+        public TextView getTvDueDate() {
+            return tvDueDate;
+        }
+        public TextView getTvPriority() {
+            return tvPriority;
+        }
+        public Button getDeleteButton() {
+            return deleteButton;
+        }
+    }
+
+    public void setOnItemClickListener(View.OnClickListener itemClickListener) {
+        mOnItemClickListener = itemClickListener;
+    }
+
+    @NonNull
+    @Override
+    public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View v = LayoutInflater.from(parent.getContext()).inflate
+                (R.layout.list_item, parent, false);
+        return new TaskViewHolder(v);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, final int position) {
+        TaskViewHolder tvh = (TaskViewHolder) holder;
+        // this line updates the information on the viewHolder based on its position
+        // (I'm assuming it has to be the last position)
+        tvh.getTvSubject().setText(taskData.get(position).getSubject());
+        tvh.getTvDueDate().setText(String.valueOf(taskData.get(position).getDueDate()));
+        tvh.getTvPriority().setText(taskData.get(position).getPriority());
+
+        if (isDeleting) {
+            tvh.getDeleteButton().setVisibility(View.VISIBLE);
+            tvh.getDeleteButton().setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    deleteItem(holder.getAdapterPosition());
+                }
+            });
+        }else {
+            // If not deleting, hide delete button
+            tvh.getDeleteButton().setVisibility(View.GONE);
+            // Remove click listener to prevent any accidental clicks
+            tvh.getDeleteButton().setOnClickListener(null);
+        }
+    }
+
+    public void  setDelete(boolean b) {
+        isDeleting = b;
+    }
+
+    private void deleteItem(int position) {
+        Task task =taskData.get(position);
+        TaskDataSource ds = new TaskDataSource(parentContext);
+
+        try {
+            ds.open();
+            boolean didDelete = ds.deleteTask(task.getTaskID());
+            ds.close();
+
+            if (didDelete) {
+                taskData.remove(position);
+                notifyDataSetChanged();
+            }
+            else {
+                Toast.makeText(parentContext, "Delete Failed!", Toast.LENGTH_LONG).show();
+            }
+        }
+        catch (Exception e) {
+            Toast.makeText(parentContext, "Delete Failed!", Toast.LENGTH_LONG).show();
+        }
+    }
+
+    // when the adapter is set this method is called
+    @Override
+    public int getItemCount() {
+        return taskData.size();
+    }
 }
--- app/src/main/java/com/example/todolist/TaskList.java
+++ app/src/main/java/com/example/todolist/TaskList.java
@@ -9,20 +9,65 @@ import android.widget.CompoundButton;
 import android.widget.ImageButton;
 import android.widget.Switch;
 import android.widget.Toast;
-
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
-
 import java.util.ArrayList;
 
 public class TaskList extends AppCompatActivity {
+    private TaskAdapter ta;
+    private ArrayList<Task> tasks;
+    private View.OnClickListener onItemClickListener = new View.OnClickListener() {
+
+        @Override
+        public void onClick(View v) {
+            RecyclerView.ViewHolder viewHolder = (RecyclerView.ViewHolder) v.getTag();
+            int position = viewHolder.getAdapterPosition();
+            int taskID = tasks.get(position).getTaskID();
+            Intent intent = new Intent(TaskList.this, MainActivity.class);
+            intent.putExtra("taskID", taskID);
+            startActivity(intent);
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tasklist);
         initNavbarButtons();
+        initAddTaskButton();
+        initDeleteSwitch();
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+
+        String sortBy = getSharedPreferences("MyTaskListPreferences", Context.MODE_PRIVATE).getString("sortfield", "subject");
+        String sortOrder = getSharedPreferences("MyTaskListPreferences", Context.MODE_PRIVATE).getString("sortorder", "ASC");
+
+        TaskDataSource ds = new TaskDataSource(this);
+
+        try {
+            ds.open();
+            tasks = ds.getTasks(sortBy, sortOrder);
+            ds.close();
+            if (tasks.size() > 0) {
+                RecyclerView contactList = findViewById(R.id.rvTask);
+                RecyclerView.LayoutManager lm = new LinearLayoutManager(this);
+                contactList.setLayoutManager(lm);
+                ta = new TaskAdapter(tasks, TaskList.this );
+                contactList.setAdapter(ta);
+                ta.setOnItemClickListener(onItemClickListener);
+            }
+            else {
+                Intent intent = new Intent(TaskList.this, MainActivity.class);
+                startActivity(intent);
+            }
+        }
+        catch (Exception e) {
+            Toast.makeText(this, "Error retrieving tasks", Toast.LENGTH_LONG).show();
+        }
     }
 
     private void initNavbarButtons() {
@@ -49,9 +94,27 @@ public class TaskList extends AppCompatActivity {
         });
     }
 
-    @Override
-    public void onResume() {
-        super.onResume();
+    private void initAddTaskButton() {
+        Button newConTact = findViewById(R.id.addBtn);
+        newConTact.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(TaskList.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+    private void initDeleteSwitch() {
+        Switch s = findViewById(R.id.switchDeleteTask);
+        s.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                Boolean status = buttonView.isChecked();
+                ta.setDelete(status);
+                ta.notifyDataSetChanged();
+            }
+        });
     }
 
 }
